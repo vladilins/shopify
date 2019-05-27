ba507258e44d62afcf7c9231d36b8e850082d50c
Vladislav Ilin <vladislav.ilin@hiq.ru>
2019-05-27 11:48:37 +0300

    first

---
+++ fashion-html-template/script.js
@@ -0,0 +1,63 @@
+$('.btn.edit-address').click(function() {
+  $(this).hide();
+  $(this).parents('td').siblings('.address-edit').show();
+  $(this).siblings('.btn.edit-address-cancel').show();
+  $(this).siblings('.btn.edit-address-submit').show();
+  $(this).siblings('.btn.delete-address').hide();
+  $(this).parents('td').siblings('.address-record').hide()
+});
+
+$('.btn.edit-address-submit').click(function() {
+  $(this).hide();
+  $(this).parents('td').siblings('.address-edit').hide();
+  $(this).siblings('.btn.edit-address-cancel').hide();
+  $(this).siblings('.btn.edit-address').show();
+  $(this).siblings('.btn.delete-address').show();
+  $(this).parents('td').siblings('.address-record').show()
+});
+
+$('.btn.edit-address-cancel').click(function() {
+  $(this).hide();
+  $(this).parents('td').siblings('.address-edit').hide();
+  $(this).siblings('.btn.edit-address-submit').hide();
+  $(this).siblings('.btn.edit-address').show();
+  $(this).siblings('.btn.delete-address').show();
+  $(this).parents('td').siblings('.address-record').show()
+});
+
+$('.carousel').carousel()
+
+$('.single-product-images').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  customPaging : function(slider, i) {
+    var thumb = $(slider.$slides[i]).data('thumb');
+    return `<a><img class="img-fluid" src=${thumb} /></a>`;
+  },
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
